d3650a1208428655440cc067859e383e7adb0e63
mustapayev <mustapayev@publicplan.de>
2024-11-29 15:52:45 +0100

    fix issue #1258 JWTCookieProvider does set flags cookie flags when value is false

--- Tests/Security/Http/Cookie/JWTCookieProviderTest.php
+++ Tests/Security/Http/Cookie/JWTCookieProviderTest.php
@@ -4,6 +4,7 @@ namespace Lexik\Bundle\JWTAuthenticationBundle\Tests\Security\Http\Cookie;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Cookie\JWTCookieProvider;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Cookie;
 
 /**
  * JWTCookieProviderTest.
@@ -34,5 +35,101 @@ class JWTCookieProviderTest extends TestCase
         $cookie = $cookieProvider->createCookie("header.payload.signature");
 
         $this->assertSame(0, $cookie->getExpiresTime());
+    }
+
+    /**
+     * @dataProvider createCookieFlagDataProvider
+     */
+    public function testCreateCookieHttpOnlyFlag(bool $defaultHttpOnlyFlag, bool $httpOnlyParam, bool $expectedFlag): void
+    {
+        $cookieProvider = new JWTCookieProvider(
+            "default_name",
+            0,
+            Cookie::SAMESITE_LAX,
+            '/',
+            null,
+            true,
+            $defaultHttpOnlyFlag
+        );
+        $cookie = $cookieProvider->createCookie(
+            "header.payload.signature",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            $httpOnlyParam
+        );
+
+        $this->assertSame($expectedFlag, $cookie->isHttpOnly());
+    }
+
+    /**
+     * @dataProvider createCookieFlagDataProvider
+     */
+    public function testCreateCookieSecureFlag(bool $defaultSecureFlag, bool $secureParam, bool $expectedFlag): void
+    {
+        $cookieProvider = new JWTCookieProvider(
+            "default_name",
+            0,
+            Cookie::SAMESITE_LAX,
+            '/',
+            null,
+            $defaultSecureFlag
+        );
+        $cookie = $cookieProvider->createCookie(
+            "header.payload.signature",
+            null,
+            null,
+            null,
+            null,
+            null,
+            $secureParam
+        );
+
+        $this->assertSame($expectedFlag, $cookie->isSecure());
+    }
+
+    /**
+     * @dataProvider createCookieFlagDataProvider
+     */
+    public function testCreateCookiePartitionedFlag(bool $defaultPartitionedFlag, bool $parititionedParam, bool $expectedFlag): void
+    {
+        $cookieProvider = new JWTCookieProvider(
+            "default_name",
+            0,
+            Cookie::SAMESITE_LAX,
+            '/',
+            null,
+            true,
+            true,
+            [],
+            $defaultPartitionedFlag
+        );
+        $cookie = $cookieProvider->createCookie(
+            "header.payload.signature",
+            null,
+            null,
+            null,
+            null,
+            null,
+            true,
+            true,
+            [],
+            $parititionedParam
+        );
+
+        $this->assertSame($expectedFlag, $cookie->isPartitioned());
+    }
+
+    public static function createCookieFlagDataProvider(): array
+    {
+        return [
+            [true, true, true],
+            [false, false, false],
+            [true, false, false],
+            [false, true, true],
+        ];
     }
 }
